93d120ba052cf9fc51e76292502f6ff8b1f87f81
Julien CAUCHETEUX <jcmcshopy@gmail.com>
2022-02-12 22:25:35 +0100

    Updated script

--- tester.sh
+++ tester.sh
@@ -1,41 +1,53 @@
 #Use https://www.dcode.fr/generateur-permutations to retrieve all the combinations
 #Put them in a file called test_values, separated by a \n
-#Change YOUR_NUMBER in results() and moves() by the number of lines in test_values
+#Change 120 in results() and moves() by the number of lines in test_values
 
 #This part is used to check if all the combinations are OK or KO
-#Use 'sh tester.sh 1' in the terminal to retrieve results.txt file
+#Use 'sh tester.sh results' in the terminal to retrieve results.txt file
 
 results()
 {
-    for i in {1..YOUR_NUMBER}
+    for i in {1..120}
     do
         ./push_swap $(sed -n "$i"p test_values) | ./checker_Mac $(sed -n "$i"p test_values) | cat >> results.txt
     done
 }
 
 #This part is used to check the number of moves your program used to sort the numbers
-#Use 'sh tester.sh 2' in the terminal to retrieve moves.txt file
+#Use 'sh tester.sh moves' in the terminal to retrieve moves.txt file
 
 moves()
 {
-    for i in {1..YOUR_NUMBER}
+    for i in {1..120}
     do
-        ./push_swap $(sed -n "$i"p test_values) | wc -l | cat >> moves.txt
+        ./push_swap $(sed -n "$i"p test_values) | wc -l | tr -d ' ' | cat >> moves.txt
     done 
 }
 
 if [ "$#" -ne 1 ]
 then
-    echo "Please use a correct format: 'sh tester.sh 1' or 'sh tester.sh 2'\n"
+    echo "Please use a correct format. Use the GitHub docs"
     exit 1
 fi
 
-if (($1 == 1))
+if [ "$1" == "results" ]
 then
     results
-elif (($1 == 2))
+elif [ "$1" == "moves" ]
 then
     moves
+elif [ "$1" == "average" ]
+then
+	moves
+	lines=$( cat moves.txt | wc -l )
+	average=$( awk '{s+=$1} END {print s}' moves.txt )
+	result=$((average))/$((lines))
+	printf $((result))
+	rm -rf moves.txt
+elif [ "$1" == "remove" ]
+then
+	rm -rf moves.txt
+	rm -rf results.txt
 else
-    echo "Please use a correct format: 'sh tester.sh 1' or 'sh tester.sh 2'\n"
+    echo "Please use a correct format. Use the GitHub docs"
 fi
