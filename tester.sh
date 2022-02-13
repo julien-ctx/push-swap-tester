d6f8756916e17f11af5d7ab8f877d2e3e2c5cc37
julien-ctx <91915468+julien-ctx@users.noreply.github.com>
2022-02-13 17:45:21 +0100

    Update tester.sh

--- tester.sh
+++ tester.sh
@@ -1,13 +1,10 @@
-#Use https://www.dcode.fr/generateur-permutations to retrieve all the combinations
-#Put them in a file called test_values, separated by a \n
-#Change YOUR_NUMBER in results() and moves() by the number of lines in test_values
-
 #This part is used to check if all the combinations are OK or KO
 #Use 'sh tester.sh results' in the terminal to retrieve results.txt file
 
 results()
 {
-    for i in {1..YOUR_NUMBER}
+    l=$(($( cat test_values | wc -l | tr -d ' ' )))
+    for ((i=1;i<=l;i++))
     do
         ./push_swap $(sed -n "$i"p test_values) | ./checker_Mac $(sed -n "$i"p test_values) | cat >> results.txt
     done
@@ -18,34 +15,37 @@ results()
 
 moves()
 {
-    for i in {1..YOUR_NUMBER}
+    l=$(($( cat test_values | wc -l | tr -d ' ' )))
+    for ((i=1;i<=l;i++))
     do
         ./push_swap $(sed -n "$i"p test_values) | wc -l | tr -d ' ' | cat >> moves.txt
     done 
 }
 
-if [ "$#" -ne 1 ]
-then
-    echo "Please use a correct format. Use the GitHub docs"
-    exit 1
-fi
-
 if [ "$1" == "results" ]
 then
+	rm -rf results.txt
+	sh combination.sh $2 | cat > test_values
     results
 elif [ "$1" == "moves" ]
 then
+	rm -rf moves.txt
+	sh combination.sh $2 | cat > test_values
     moves
 elif [ "$1" == "average" ]
 then
+	rm -rf test_values
+	sh combination.sh $2 | cat > test_values
 	moves
 	lines=$( cat moves.txt | wc -l )
 	average=$( awk '{s+=$1} END {print s}' moves.txt )
 	result=$((average))/$((lines))
 	printf $((result))
 	rm -rf moves.txt
+	rm -rf test_values
 elif [ "$1" == "remove" ]
 then
+	rm -rf test_values
 	rm -rf moves.txt
 	rm -rf results.txt
 else
